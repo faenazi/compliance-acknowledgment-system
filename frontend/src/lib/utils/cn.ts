1732dbbc45e4f966102e73979b00e7daceaa2cc6
faenazi <99114612+faenazi@users.noreply.github.com>
2026-04-16 16:05:57 +0300

    Delete frontend directory

--- frontend/src/lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Combine conditional class values and de-duplicate Tailwind utilities.
- */
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}
